1031f7f3e598c7f84a759abcce0beb87963499e0
shravan <shravan.318@gmail.com>
2019-10-05 12:53:42 +0530

    add delete task and task filter functionality

--- app.js
+++ app.js
@@ -9,18 +9,26 @@ const taskInput = document.querySelector("#task");
 loadEventListeners();
 
 function loadEventListeners() {
-    form.addEventListener("submit", addTask)
+    form.addEventListener("submit",addTask)
+    // remove task 
+    taskList.addEventListener("click",removeTask)
+    //clear tasks event
+    cleatBtn.addEventListener("click",clearTasks)
+    //filter tasks
+    filter.addEventListener("keyup",filtertasks)
 }
-function addTask(e){
+function addTask(e) {
     e.preventDefault();
 
-    if(taskInput.value === ""){
-        alert("Please add a task")
+    if (taskInput.value === "") {
+        alert("Please enter a task")
+        return
     }
+
     // create a li
     const li = document.createElement("li");
     //add class to li
-    li.className= 'collection-item';
+    li.className = 'collection-item';
     //create text node
     li.appendChild(document.createTextNode(taskInput.value));
 
@@ -28,11 +36,42 @@ function addTask(e){
     link.className = 'delete-item secondary-content';
     link.innerHTML = '<i class="fa fa-remove"></i>';
     li.appendChild(link);
-    console.log(li)
 
     // appenmd li to ul
     taskList.appendChild(li)
 
-    taskInput = ""
+    taskInput.value = ""
+
+
+}
+
+//Remove Task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains("delete-item")) {
+        if (confirm("Are you sure")) {
+            e.target.parentElement.parentElement.remove()
+        }
+    }
+}
+
+// clear tasks
+function clearTasks() {
+
+    // taskList.innerHTML = ""
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+function filtertasks(e){
+    const text = e.target.value.toLowerCase();
+   document.querySelectorAll(".collection-item").forEach(function(task){
+const item = task.firstChild.textContent;
+if(item.toLowerCase().indexOf(text) != -1){
+    task.style.display = "block"
+}else(
+    task.style.display = "none"
+    )
+   })
 
 }
